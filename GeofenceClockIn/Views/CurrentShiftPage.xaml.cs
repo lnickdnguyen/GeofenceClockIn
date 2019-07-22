34264e809998b0d57d3c804a01597b65c0c1e5c3
Jarod Aerts <jarod2017@gmail.com>
2019-07-22 11:03:40 -0700

    Did a bunch of stuff

---
+++ GeofenceClockIn/Views/CurrentShiftPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GeofenceClockIn.Views
+{
+    public partial class CurrentShiftPage : ContentView
+    {
+        public CurrentShiftPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
