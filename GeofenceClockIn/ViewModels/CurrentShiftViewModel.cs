aed5e0c49c5a2bd3ce9d5bd620127e4c27766874
Luke Bogus <t-lubogu@microsoft.com>
2019-07-23 14:02:02 -0700

    Enabled toggling of start or end shift screen

--- GeofenceClockIn/ViewModels/CurrentShiftViewModel.cs
+++ GeofenceClockIn/ViewModels/CurrentShiftViewModel.cs
@@ -6,9 +6,25 @@ namespace GeofenceClockIn.ViewModels
 {
     public class CurrentShiftViewModel : BaseViewModel
     {
+        private bool _isStartShiftActive;
+        public bool IsStartShiftActive
+        {
+            get => _isStartShiftActive;
+            set { SetProperty(ref _isStartShiftActive, value); }
+        }
+
+        private bool _isEndShiftActive;
+        public bool IsEndShiftActive
+        {
+            get => _isEndShiftActive;
+            set { SetProperty(ref _isEndShiftActive, value); }
+        }
+
         public CurrentShiftViewModel()
         {
             Title = "hello";
+            _isStartShiftActive = true;
+            _isEndShiftActive = false;
         }
     }
 }
