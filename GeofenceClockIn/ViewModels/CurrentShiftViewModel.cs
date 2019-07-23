28560278753c2d8036bb8a4b235e958011f512a5
Luke Bogus <t-lubogu@microsoft.com>
2019-07-23 11:58:45 -0700

    Set up ViewModel

---
+++ GeofenceClockIn/ViewModels/CurrentShiftViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeofenceClockIn.ViewModels
+{
+    public class CurrentShiftViewModel : BaseViewModel
+    {
+        public CurrentShiftViewModel()
+        {
+            Title = "hello";
+        }
+    }
+}
