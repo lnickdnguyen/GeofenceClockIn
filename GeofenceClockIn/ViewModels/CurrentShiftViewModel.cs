7a0019d9c2ab0fdb2b355eaa7b234005c5626494
Luke Bogus <t-lubogu@microsoft.com>
2019-07-23 14:52:25 -0700

    Corrected view model error

--- GeofenceClockIn/ViewModels/CurrentShiftViewModel.cs
+++ GeofenceClockIn/ViewModels/CurrentShiftViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace GeofenceClockIn.ViewModels
 {
@@ -20,9 +21,28 @@ namespace GeofenceClockIn.ViewModels
             set { SetProperty(ref _isEndShiftActive, value); }
         }
 
+        public Command StartShiftCommand { get; set; }
+        public Command EndShiftCommand { get; set; }
+
         public CurrentShiftViewModel()
         {
             Title = "hello";
+
+            _isStartShiftActive = true;
+            _isEndShiftActive = false;
+
+            StartShiftCommand = new Command(OnStartShift);
+            EndShiftCommand = new Command(OnEndShift);
+        }
+
+        private void OnStartShift()
+        {
+            IsStartShiftActive = false;
+            IsEndShiftActive = true;
+        }
+
+        private void OnEndShift()
+        {
             _isStartShiftActive = true;
             _isEndShiftActive = false;
         }
