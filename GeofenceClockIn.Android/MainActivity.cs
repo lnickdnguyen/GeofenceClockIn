714f1d9e896a2780f02516623ea1d9d76997f285
Luke Bogus <t-lubogu@microsoft.com>
2019-07-23 18:39:21 -0700

    Trying to get the API token to work, but it just doesn't :(

--- GeofenceClockIn.Android/MainActivity.cs
+++ GeofenceClockIn.Android/MainActivity.cs
@@ -34,7 +34,11 @@ namespace GeofenceClockIn.Droid
             global::Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "Visual_Experimental", "CollectionView_Experimental", "FastRenderers_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState); //Initialize Mapview
             LoadApplication(new App());
+
+            
+            
 
             StartGeofencePlugin();
         }
